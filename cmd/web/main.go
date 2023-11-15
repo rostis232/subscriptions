97b2fe30b277bea616f360ec2f21f14f62272cfe
Rostyslav Pylypiv <rostislav.pylypiv@gmail.com>
2023-11-16 01:09:10 +0200

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const webPort = "8084"
+
+func main(){
+	//connect to the DB
+	
+	//sreate sessions
+	
+	//create channels
+	
+	//create waitgroup
+	
+	//set up the applications config
+	
+	//set up mail
+	
+	//listen for web connections
+	
+}
